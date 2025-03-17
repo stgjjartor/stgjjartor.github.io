63e890b7e668315028dfe918e7a2c1591b5c59b8
stgjjartor <stgjartor@gmail.com>
2025-03-17 13:05:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const comenzarBtn = document.getElementById('comenzarBtn');
     const pagina1 = document.getElementById('pagina1');
     const pagina2 = document.getElementById('pagina2');
+    const verMenuBtn = document.getElementById('verMenuBtn');
+    const pedidoChatBtn = document.getElementById('pedidoChatBtn');
 
     // Agregar el evento de clic para el botón "Comenzar"
     comenzarBtn.addEventListener('click', function () {
         pagina1.classList.add('hidden');  // Ocultar la página 1
         pagina2.classList.remove('hidden');  // Mostrar la página 2
+    });
+
+    // Evento para el botón "Ver Menú" para redirigir a la página del menú
+    verMenuBtn.addEventListener('click', function () {
+        window.location.href = 'https://stgjjartor.github.io/index.html';  // Redirigir al menú
+    });
+
+    // Evento para el botón "Pedido Chat" para redirigir a la página del chat
+    pedidoChatBtn.addEventListener('click', function () {
+        window.location.href = 'https://stgjjartor.github.io/personal.html';  // Redirigir al chat
     });
 
     // Configuración para el botón "Volver" en la Página 3
