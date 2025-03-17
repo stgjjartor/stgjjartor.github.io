7a18475eda70443fc45a949c9de717eaff2a4b07
stgjjartor <stgjartor@gmail.com>
2025-03-17 12:54:52 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,17 @@
-// Importar las funciones necesarias de Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getDatabase, ref, push, onChildAdded } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
 
-// Configuración de Firebase
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBE00qPqjZQxCyFVPVeTGBBmjTm-tQL-bw",
-  authDomain: "lacocinadeana-d99e4.firebaseapp.com",
-  databaseURL: "https://lacocinadeana-d99e4-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "lacocinadeana-d99e4",
-  storageBucket: "lacocinadeana-d99e4.firebasestorage.app",
-  messagingSenderId: "524705575432",
-  appId: "1:524705575432:web:75211ec85fe2d4b52a57bd",
-  measurementId: "G-CV273FDWV9"
+    apiKey: "AIzaSyBE00qPqjZQxCyFVPVeTGBBmjTm-tQL-bw",
+    authDomain: "lacocinadeana-d99e4.firebaseapp.com",
+    databaseURL: "https://lacocinadeana-d99e4-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "lacocinadeana-d99e4",
+    storageBucket: "lacocinadeana-d99e4.firebasestorage.app",
+    messagingSenderId: "524705575432",
+    appId: "1:524705575432:web:75211ec85fe2d4b52a57bd",
+    measurementId: "G-CV273FDWV9"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-// Exportar la base de datos para su uso en otros archivos
-export { database, ref, push, onChildAdded };
+const analytics = getAnalytics(app);
